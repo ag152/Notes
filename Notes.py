76ded71131e3eeef5d24185de9833b748e5dd6a4
ag152 <33761386+ag152@users.noreply.github.com>
2017-11-28 09:31:13 -0800

    Initial commit

---
+++ Notes.py
@@ -0,0 +1,74 @@
+print("Hello world!")
+
+#Alejandro Gutierrez
+
+a = 7
+b = 5
+c = 9
+
+print(a + b)
+
+print(c * b - a)
+
+print(c ** a / b)
+
+print(13 % 12)
+
+# the "%" sign is a modulus. It finds the reminder.
+
+car_name = "The Wiebe Moble"
+car_type = "BMW"
+car_cylinders = 8
+car_mpg = 5000.9
+
+print("I have a car called %s. It's pretty nice." % car_name)
+print("I have a car called %s. It's a %s." % (car_name, car_type)) # watch the order
+
+print("What is your name?")
+
+name = input(">_ ")
+
+print("Hello %s," % name)
+
+age = input("How are old are you?")
+
+print("%s?! That's old really old. You belong in a retirement home.")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
