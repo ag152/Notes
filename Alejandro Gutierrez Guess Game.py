f7445ffda780487c537845832addbd68e810cd8f
ag152 <33761386+ag152@users.noreply.github.com>
2017-12-11 09:35:26 -0800

    completed
    finished

--- Alejandro Gutierrez Guess Game.py
+++ Alejandro Gutierrez Guess Game.py
@@ -1,18 +1,43 @@
-#Generate a number
-#Ask the user for an input(number)
-#Does the guess match the number?
-#Add "Higher" and "Lower"
-#Add 5 guesses
+# Generate a number
+# Ask the user for an input(number)
+# Does the guess match the number?
+# Add "Higher" and "Lower"
+# Add 5 guesses
 
 
 import random
 
-print("What is your guess?")
-guess = input(">_")
+guessesTaken = 0
+number = random.randint(1, 5)
 
-print(random.randint(1, 50))
+global guess
 
 
+while guessesTaken < 5:
+    print("Take a guess.")
+    guess = input()
+    guess = int(guess)
 
+    guessesTaken = guessesTaken + 1
+
+    if guess < number:
+     print("Your guess is too low.")
+
+    if guess > number:
+     print("Your guess is too high.")
+
+    if guess == number:
+     break
+
+if guess == number:
+    guessesTaken = str(guessesTaken)
+    print("Good job.")
+
+if guess != number:
+    number = str(number)
+    print("You lost.")
+
+
+print("It was %s." % number)
 
 
--- Warmups.py
+++ Warmups.py
@@ -1,5 +1,29 @@
-def add_py(name):
-    print(name + ".py")
+# def add_py(name):
+#     print(name + ".py")
+#
+#
+#     print(add_py)
 
 
-    print(add_py)
+# def add(first_num, second_num, thrid_num):
+#     print(first_num + second_num + thrid_num)
+
+
+# add(15, 18, 9000)
+# add(80, 90, 100)
+
+# def repeat(string):
+#     print("Hello")
+#     print("Hello")
+#     print("Hello")
+
+
+# repeat
+
+
+
+# def date(month, day, year):
+#     print(str(month) +"/" +"/" + str(day) +"/"+ str(year))
+#
+#
+# date(12, 8, 17)
